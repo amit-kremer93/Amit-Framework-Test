f8ab8a735dd0e2e8fbe7a63ea6a0dfd5cf8ace4e
amit.kremer <amit.kremer@appsflyer.com>
2021-09-09 13:56:37 +0300

    initial

---
+++ Amit-Framework-Test/Amit-Framework-Test/Amit_Framework_Test.h
@@ -0,0 +1,18 @@
+//
+//  Amit_Framework_Test.h
+//  Amit-Framework-Test
+//
+//  Created by Amit Kremer on 09/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Amit_Framework_Test.
+FOUNDATION_EXPORT double Amit_Framework_TestVersionNumber;
+
+//! Project version string for Amit_Framework_Test.
+FOUNDATION_EXPORT const unsigned char Amit_Framework_TestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Amit_Framework_Test/PublicHeader.h>
+
+
---
+++ Amit-Framework-Test/Amit-Framework-Test/MyExtension.swift
@@ -0,0 +1,14 @@
+//
+//  MyExtension.swift
+//  Amit-Framework-Test
+//
+//  Created by Amit Kremer on 09/09/2021.
+//
+
+import Foundation
+
+public extension Array {
+    func printCount() {
+        print("~~>> \(self.count)")
+    }
+}
